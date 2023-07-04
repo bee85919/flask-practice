b41b4d6b665f7a5486e4b515ceeb718cca812a17
bee <bee85919@gmail.com>
2023-07-05 00:31:09 +0900

    Add feat. write

--- server.py
+++ server.py
@@ -1,17 +1,15 @@
-from flask import Flask
-
+from flask import Flask, request, redirect
+ 
 app = Flask(__name__)
-
-# 아래 코드는 복수의 데이터를 파이썬의 데이터로 전환
-# 동급의 데이터(각각의 글들)은 리스트로 표현
-# 데이터의 속성(id, title, body)은 딕셔너리로 표현
+ 
+ 
 topics = [
     {'id': 1, 'title': 'html', 'body': 'html is ...'},
     {'id': 2, 'title': 'css', 'body': 'css is ...'},
     {'id': 3, 'title': 'javascript', 'body': 'javascript is ...'}
 ]
-
-# HTML 템플릿 생성 함수
+ 
+ 
 def template(contents, content):
     return f'''<!doctype html>
     <html>
@@ -21,26 +19,28 @@ def template(contents, content):
                 {contents}
             </ol>
             {content}
+            <ul>
+                <li><a href="/create/">create</a></li>
+            </ul>
         </body>
     </html>
     '''
-
-# 토픽 리스트 HTML 생성 함수 # dynamic
+ 
+ 
 def getContents():
     liTags = ''
     for topic in topics:
         liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
     return liTags
-
-# 메인 페이지 라우팅 
+ 
+ 
 @app.route('/')
 def index():
     return template(getContents(), '<h2>Welcome</h2>Hello, WEB')
-
-# 독립된 페이지 라우팅 
+ 
+ 
 @app.route('/read/<int:id>/')
 def read(id):
-    # 토픽 제목과 내용을 찾기 위해 리스트를 순회
     title = ''
     body = ''
     for topic in topics:
@@ -48,13 +48,58 @@ def read(id):
             title = topic['title']
             body = topic['body']
             break
-    # 작성된 제목과 내용을 템플릿에 적용
     return template(getContents(), f'<h2>{title}</h2>{body}')
+ 
 
-# 새 페이지 생성 라우팅 
-@app.route('/create/')
+@app.route('/create/', methods=['GET', 'POST'])
+# - GET: browser가 정보를 가져올 때 사용
+# - POST: browser가 정보를 변경할 때 사용
 def create():
-    return 'Create'
+    if request.method == 'GET':
+        '''
+        1. 서버로 전송할 사용자의 입력 정보를 form 태그로 그룹핑한다.
 
-# Flask 애플리케이션 실행
+        2. <form method="XXX"> 
+            - 여기에선 GET을 사용한다.         
+                - URL으로 입력 정보를 전달할 땐 입력 정보 노출에 주의
+
+        3. <input type="submit"> 
+            - submit 버튼 
+            → form의 입력 정보 
+            → form의 action 
+            → method 
+            → 전송
+        '''
+        content = '''
+            <form action="/create/" method="POST">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="body" placeholder="body"></textarea></p>
+                <p><input type="submit" value="create"></p>
+            </form>
+        '''
+        return template(getContents(), content)
+    
+    elif request.method == 'POST':
+        '''
+        1. POST: http 통신의 body를 통해 전송
+            → 입력 정보를 감추고, 대용량의 데이터를 전송 할 수 있다. 
+
+        2. browser의 요청 정보를 확인하기 위해 flask.request 모듈 이용. 
+
+        3. request.method: browser가 전송한 method를 확인할 수 있다. 
+
+        4. request.form['NAME']: browser가 전송한 POST 데이터를 확인할 수 있다. 
+        '''
+        global nextId
+
+        title,body = request.form['title'], request.form['body']
+
+        newTopic = {'id': nextId, 'title': title, 'body': body}
+        topics.append(newTopic)
+
+        url = '/read/'+str(nextId)+'/'
+        nextId = nextId + 1
+        return redirect(url)
+ 
+ 
 app.run(debug=True)
